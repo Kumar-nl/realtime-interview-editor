22c70ba661f6b547636b51105a2def9e6aa06431
Kumar Sagar <kumar.sagar.ks6778106@gmail.com>
2023-08-25 23:06:06 +0530

    frontend of the interview editor project

---
+++ src/pages/EditorPage.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import Client from "../components/Client";
+import Editor from "../components/Editor";
+
+const EditorPage = () => {
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Sagar" },
+    { socketId: 2, username: "Aashu" },
+  ]);
+
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+        <button className="btn copyBtn">Copy Room ID</button>
+        <button className="btn leaveBtn">Leave</button>
+      </div>
+      <div className="editorWrap">
+        <Editor />
+      </div>
+    </div>
+  );
+};
+
+export default EditorPage;
