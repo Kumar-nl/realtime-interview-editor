107f25f1ef44ba0acb43185cc88f186a6c4bf6d8
Kumar Sagar <kumar.sagar.ks6778106@gmail.com>
2023-09-02 13:42:38 +0530

    backend development done

--- server.js
+++ server.js
@@ -1,17 +1,76 @@
-const { log } = require("console");
 const express = require("express");
 const http = require("http");
-const {Server} = require("socket.io");
+const socketio = require("socket.io");
+const ACTIONS = require("./src/Actions");
+const cors = require("cors");
+const path = require('path');
 
 const app = express();
-const server = http.createServer(app);
+app.use(express.static('build'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 const PORT = process.env.PORT || 5000;
-const io = new Server(server);
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+const userSocketMap = {};
+
+const getAllConnectedClients = (roomId) => {
+    // io.sockets.adapter.rooms.get returns us a map
+    // so to convert it in Array we are using Array.from
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
+        return {
+            socketId,
+            username: userSocketMap[socketId],
+        };
+    });
+}
 
 io.on('connection', (socket) => {
     console.log('socket connected!', socket.id);
+
+    socket.on(ACTIONS.JOIN, ({roomId, username}) => {
+        userSocketMap[socket.id] = username;
+        socket.join(roomId);
+        const allClients = getAllConnectedClients(roomId);
+        console.log("Clients: ", allClients);
+        allClients.forEach(({socketId}) => {
+            io.to(socketId).emit(ACTIONS.JOINED, {
+                allClients,
+                username,
+                socketId: socket.id,
+            })
+        })
+    });
+
+    socket.on(ACTIONS.CODE_CHANGE, ({roomId, code}) => {
+        socket.in(roomId).emit(ACTIONS.CODE_CHANGE, {code})
+    })
+
+    socket.on(ACTIONS.SYNC_CODE, ({socketId, code}) => {
+        io.to(socketId).emit(ACTIONS.CODE_CHANGE, {code})
+    })
+
+    socket.on('disconnecting', () => {
+        const rooms = [...socket.rooms];
+        rooms.forEach((roomId) => {
+            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+                socketId:socket.id,
+                username:userSocketMap[socket.id],
+            })
+        })
+        delete userSocketMap[socket.id];
+        socket.leave();
+    })
 })
 
+app.get("/", (req, res) => {
+    res.send("Hello, i am testing")
+})
 
 server.listen(PORT, ()=> {
     console.log(`listening on port ${PORT}`);
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,13 +1,94 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  useLocation,
+  useNavigate,
+  Navigate,
+  useParams,
+} from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 import Client from "../components/Client";
 import Editor from "../components/Editor";
+import {initSocket}  from "../socket";
+const ACTIONS = require("../Actions");
 
 const EditorPage = () => {
-  const [clients, setClients] = useState([
-    { socketId: 1, username: "Sagar" },
-    { socketId: 2, username: "Aashu" },
-  ]);
+  const [clients, setClients] = useState([]);
+  const socketRef = useRef(null);
+  const codeRef = useRef(null);
+  const location = useLocation();
+  const { roomId } = useParams();
+  const reactNavigator = useNavigate();
+
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log("socket error ", e);
+        toast.error("Socket connection failed, try again later.");
+        reactNavigator("/");
+      }
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+
+      // Listening for JOINED event
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ allClients, username, socketId }) => {
+          console.log("Clients: ", allClients);
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined!`);
+          }
+          setClients(allClients);
+          socketRef.current.emit(ACTIONS.SYNC_CODE, {
+            code: codeRef.current,
+            socketId
+          });
+        }
+      );
+
+      // Listening for disconnected
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({socketId, username}) => {
+        toast.success(`${username} left the room.`)
+        setClients((prev) => {
+          return prev.filter(client => client.socketId !== socketId);
+        })
+      })
+
+    };
+
+    init();
+
+    return () => {
+      socketRef.current.disconnect();
+      socketRef.current.off(ACTIONS.JOINED)
+      socketRef.current.off(ACTIONS.DISCONNECTED)
+    }
+  }, []);
+
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
+
+  async function copyRoomId() {
+    try {
+      await navigator.clipboard.writeText(roomId);
+      toast.success('Room ID has been copied');
+    } catch (err) {
+      toast.error('Could not copy the Room Id')
+      console.log(err);
+    }
+  }
+
+  async function leaveRoom() {
+    reactNavigator('/');
+  }
 
   return (
     <div className="mainWrap">
@@ -20,11 +101,11 @@ const EditorPage = () => {
             ))}
           </div>
         </div>
-        <button className="btn copyBtn">Copy Room ID</button>
-        <button className="btn leaveBtn">Leave</button>
+        <button className="btn copyBtn" onClick= {copyRoomId}>Copy Room ID</button>
+        <button className="btn leaveBtn" onClick= {leaveRoom}>Leave</button>
       </div>
       <div className="editorWrap">
-        <Editor />
+        <Editor socketRef={socketRef} roomId={roomId} onCodeChange={(code) => {codeRef.current = code;}} />
       </div>
     </div>
   );
